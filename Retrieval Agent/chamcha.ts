7df46b1fded5b17b482d25325f9f92f5873f1e9a
Shashwat <shash01nautiyal@gmail.com>
2025-01-23 15:06:03 +0530

    library configured

---
+++ Retrieval Agent/chamcha.ts
@@ -0,0 +1,42 @@
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { OpenAIEmbeddings } from "@langchain/openai";
+
+import { Annotation } from "@langchain/langgraph";
+import { BaseMessage } from "@langchain/core/messages";
+
+const urls = [
+  "https://lilianweng.github.io/posts/2023-06-23-agent/",
+  "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+  "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+];
+
+const docs = await Promise.all(
+  urls.map((url) => new CheerioWebBaseLoader(url).load()),
+);
+const docsList = docs.flat();
+
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 500,
+  chunkOverlap: 50,
+});
+const docSplits = await textSplitter.splitDocuments(docsList);
+
+// Add to vectorDB
+const vectorStore = await MemoryVectorStore.fromDocuments(
+  docSplits,
+  new OpenAIEmbeddings(),
+);
+
+const retriever = vectorStore.asRetriever();
+
+// page2
+
+
+const GraphState = Annotation.Root({
+  messages: Annotation<BaseMessage[]>({
+    reducer: (x, y) => x.concat(y),
+    default: () => [],
+  })
+})
